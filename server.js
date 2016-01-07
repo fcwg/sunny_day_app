4d9c085a44a74158eeb24d5b5fb81781132f8695
Fu Wong <fcwong@yahoo.com>
2016-01-06 16:16:03 -0800

    res.send index.htm

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.htm');
 });
 
 var port = process.env.PORT || 5000;
